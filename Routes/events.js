8898a4f1c2166b54bfdf5f50a658c5880f091164
shubham <shubh0795@gmail.com>
2022-12-22 21:43:19 +0530

    pending and accepted request added

--- Routes/events.js
+++ Routes/events.js
@@ -21,6 +21,40 @@ router.get('/:id', async (req, res) => {
 }
 );
 
+router.patch('/:id/request', async (req, res) => {
+    try {
+        const event = await Event.findById(req.params.id);
+        if (!event.pending.includes(req.body.userId)) {
+            await event.updateOne({ $push: { pending: req.body.userId } });
+            res.status(200).json('The request has been sent');
+        } else {
+            res.status(403).json('You have already sent a request');
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+);
+
+router.patch('/:id/accept', async (req, res) => {
+    try {
+        const event = await Event.findById(req.params.id);
+        if (event.pending.includes(req.body.userId)) {
+            await event.updateOne({ $pull: { pending: req.body.userId } });
+            await event.updateOne({ $push: { attending: req.body.userId } });
+            res.status(200).json('The request has been accepted');
+        } else {
+            res.status(403).json('You have not sent a request yet');
+        }
+    } catch (err) {
+        res.status(500).json(err);
+
+    }
+}
+);
+
+
+
 router.post('/', async (req, res) => {
     const event = new Event({
         name: req.body.name,
