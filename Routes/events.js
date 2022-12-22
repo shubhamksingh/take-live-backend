e9faf3e9e4af6f36cea2c5e0edbdae8c585cf1ae
shubham <shubh0795@gmail.com>
2022-12-22 20:10:26 +0530

    changed the get events path

--- Routes/events.js
+++ Routes/events.js
@@ -3,7 +3,7 @@ const Event = require('../Models/events');
 
 router.get('/', async (req, res) => {
     try {
-        const events = await Event.find().populate('pending');
+        const events = await Event.find();
         res.status(200).json(events);
     } catch (err) {
         res.status(500).json(err);
