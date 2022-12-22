962a297990661cc939636d89cd73d8156bce7988
shubham <shubh0795@gmail.com>
2022-12-22 22:35:43 +0530

    ad

--- Routes/events.js
+++ Routes/events.js
@@ -25,8 +25,8 @@ router.patch('/:id/request', async (req, res) => {
     try {
         const event = await Event.findById(req.params.id);
         if (!event.pending.includes(req.body.userId)) {
-            await event.updateOne({ $push: { pending: req.body.userId } });
-            res.status(200).json({message : 'The request has been sent', event});
+            await event.updateOne({ $push: { pending: req.body.userId } }, { new: true});
+            res.status(200).json({message : 'The request has been sent', event : {...event._doc, pending: [...event.pending, req.body.userId]}});
         } else {
             res.status(403).json('You have already sent a request');
         }
